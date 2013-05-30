239b84b380787b6b2f0a4fb2b621e22e3c53feb3
Kyle Stetz <kylestetz@gmail.com>
2013-05-30 08:20:35 -0400

    Switched date formatting to YYYY-MM-DD to fix ff & safari bug

--- example/site.js
+++ example/site.js
@@ -1,10 +1,10 @@
 $(document).ready( function() {
 
 var eventArray = [
-  { date: "06-24-2013 07:52", title: "175 down, 190 to go", url: "http://google.com", time: "7:15PM" },
-  { date: "06-28-2013", title: "June 28th Part 1", url: "http://www.google.com" },
-  { date: "06-28-2013", title: "June 28th Part 2", arbitraryObject: 42 },
-  { date: "07-16-2013", title: "Somebody's Birthday." }
+  { date: "2013-06-24 07:52", title: "175 down, 190 to go", url: "http://google.com", time: "7:15PM" },
+  { date: "2013-06-28", title: "June 28th Part 1", url: "http://www.google.com" },
+  { date: "2013-06-28", title: "June 28th Part 2", arbitraryObject: 42 },
+  { date: "2013-07-16", title: "Somebody's Birthday." }
 ];
 
 $('.cal1').clndr({
