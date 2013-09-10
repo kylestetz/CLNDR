c95824a976ab2ae917811d4fd96ec0a547518cfd
Kyle Stetz <kylestetz@gmail.com>
2013-09-09 23:07:01 -0400

    new clndr demo site, woo

---
+++ js/site.js
@@ -0,0 +1,41 @@
+var clndr = {};
+
+$( function() {
+
+  // PARDON ME while I do a little magic to keep these events relevant for the rest of time...
+  var currentMonth = moment().format('YYYY-MM');
+  var nextMonth    = moment().add('month', 1).format('YYYY-MM');
+
+  var events = [
+    { date: currentMonth + '-' + '10', title: 'Persian Kitten Auction', location: 'Center for Beautiful Cats' },
+    { date: currentMonth + '-' + '19', title: 'Cat Frisbee', location: 'Jefferson Park' },
+    { date: currentMonth + '-' + '23', title: 'Kitten Demonstration', location: 'Center for Beautiful Cats' },
+    { date: nextMonth + '-' + '07',    title: 'Small Cat Photo Session', location: 'Center for Cat Photography' }
+  ];
+
+  clndr = $('#full-clndr').clndr({
+    template: $('#full-clndr-template').html(),
+    events: events
+  });
+
+  $('#mini-clndr').clndr({
+    template: $('#mini-clndr-template').html(),
+    events: events,
+    clickEvents: {
+      click: function(target) {
+        if(target.events.length) {
+          var daysContainer = $('#mini-clndr').find('.days-container');
+          daysContainer.toggleClass('show-events', true);
+          $('#mini-clndr').find('.x-button').click( function() {
+            daysContainer.toggleClass('show-events', false);
+          });
+        }
+      }
+    }
+  });
+
+  $('#clndr-3').clndr({
+    template: $('#clndr-3-template').html(),
+    events: events
+  });
+});
