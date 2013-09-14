75c3f7d32200526a416ae8afde5d668f2d082e14
Kyle Stetz <kylestetz@gmail.com>
2013-09-14 11:40:01 -0400

    added grunt workflow for minification. added jquery plugin manifest file

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      full_src: {
+        options: {
+          beautify: true,
+          mangle: false,
+          banner: '/*!              ~ CLNDR v<%= pkg.version %> ~ \n' +
+                  ' * ============================================== \n'  +
+                  ' *       https://github.com/kylestetz/CLNDR \n'        +
+                  ' * ============================================== \n'  +
+                  ' *  created by kyle stetz (github.com/kylestetz) \n'   +
+                  ' *        &available under the MIT license \n'         +
+                  ' * http://opensource.org/licenses/mit-license.php \n'  +
+                  ' * ============================================== \n'  +
+                  ' */\n'
+        },
+        files: {
+          './<%= pkg.name %>.js': 'src/<%= pkg.name %>.js'
+        }
+      },
+      mini_src: {
+        options: {
+          banner: '/*! <%= pkg.name %>.min.js v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        files: {
+          './<%= pkg.name %>.min.js': 'src/<%= pkg.name %>.js'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify']);
+
+};
--- example/site.js
+++ example/site.js
@@ -1,10 +1,14 @@
 $(document).ready( function() {
 
+  // here's some magic to make sure the dates are happening this month.
+  var thisMonth = moment().format('YYYY-MM');
+
+  // Here's our events array. We could grab this via AJAX as well.
   var eventArray = [
-    { date: "2013-07-24 07:52", title: "175 down, 190 to go", url: "http://google.com", time: "7:15PM" },
-    { date: "2013-08-28", title: "August 28th Part 1", url: "http://www.google.com" },
-    { date: "2013-08-28", title: "August 28th Part 2", arbitraryObject: 42 },
-    { date: "2013-09-16", title: "Somebody's Birthday." }
+    { date: thisMonth + "-24 07:52", title: "This is an event title", url: "http://google.com", time: "7:15PM" },
+    { date: thisMonth + "-28", title: "the 28th, Part 1", url: "http://www.google.com" },
+    { date: thisMonth + "-28", title: "the 28th, Part 2", arbitraryObject: 42 },
+    { date: thisMonth + "-16", title: "Another title", anotherObject: "clndr exposes whatever is in your event object" }
   ];
 
   var clndr1 = $('.cal1').clndr({
@@ -19,7 +23,12 @@ $(document).ready( function() {
   var clndr2 = $('.cal2').clndr({
     template: $('#template-calendar').html(),
     events: eventArray,
-    startWithMonth: moment().add('month', 1)
+    startWithMonth: moment().add('month', 1),
+    clickEvents: {
+      click: function(target) {
+        console.log(target);
+      }
+    }
   });
 
   // bind both clndrs to the left and right arrow keys
