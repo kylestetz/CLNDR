7691ab57f20132d9e4f161ad10fa20b2e952bb28
Foaly* <hi@jonathanschmid.de>
2013-09-16 15:03:17 +0100

    fixes setEvents method
    calling setEvents method threw ReferenceError: calendar is not defined

--- clndr.js
+++ clndr.js
@@ -142,7 +142,7 @@
     }, Clndr.prototype.setYear = function(newYear) {
         this.month.year(newYear), this.render();
     }, Clndr.prototype.setEvents = function(events) {
-        this.options.events = this.addMomentObjectToEvents(events), calendar.render();
+        this.options.events = this.addMomentObjectToEvents(events), this.render();
     }, Clndr.prototype.addMomentObjectToEvents = function(events) {
         var self = this, i = 0, l = events.length;
         for (i; l > i; i++) events[i]._clndrDateObject = moment(events[i][self.options.dateParameter]);
