8c2e34df56b56a8dccde3d10b3ede405f6b44445
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-10-24 17:37:58 +0200

    Use grunt to build less file and set up a watcher for LESS and JS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,11 +29,47 @@ module.exports = function(grunt) {
           './<%= pkg.name %>-<%= pkg.version %>.min.js': 'src/<%= pkg.name %>.js'
         }
       }
+    },
+    less: {
+      development: {
+       files: {
+          "example/styles/clndr.css": "example/styles/clndr.less"
+        }
+      },
+      production: {
+        options: {
+          compress: true,
+          report: "min",
+          sourceMap: true
+        },
+        files: {
+          "example/styles/clndr.css": "example/styles/clndr.less"
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      uglify: {
+        files: ['src/*.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+          debounceDelay: 1000 // Don't call uglify more than once per second
+        }
+      },
+      less: {
+        files: ['example/styles/*.less'],
+        tasks: ['less:development']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'less:production']);
 
 };
