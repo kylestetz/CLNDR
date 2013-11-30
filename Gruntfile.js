e746d409529a645344c6328dae1e83099ad64e18
Adam Prager <prager.adam87@gmail.com>
2013-11-30 23:28:05 +0100

    Remove version from minified filename

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
           banner: '/*! <%= pkg.name %>.min.js v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
         },
         files: {
-          './<%= pkg.name %>-<%= pkg.version %>.min.js': 'src/<%= pkg.name %>.js'
+          './<%= pkg.name %>.min.js': 'src/<%= pkg.name %>.js'
         }
       }
     },
