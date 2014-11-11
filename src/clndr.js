f9dfa7305d3d86f26cad6c00457563d2669e6070
Philippe Huibonhoa <phuibonhoa@gmail.com>
2014-11-10 18:16:46 -0800

    Added removeEvents function to cldnr.
    This allows users to remove events from the calendar, given a matching function.

--- src/clndr.js
+++ src/clndr.js
@@ -856,6 +856,17 @@
     return this;
   };
 
+  Clndr.prototype.removeEvents = function(matchingFunction) {
+    for (i = 0; i < this.options.events.length; i++) {
+      if(matchingFunction(this.options.events[i]) == true) {
+        this.options.events.splice(i, 1);
+      }
+    }
+
+    this.render();
+    return this;
+  };
+
   Clndr.prototype.addMomentObjectToEvents = function(events) {
     var self = this;
     var i = 0, l = events.length;
